2ba13562dde92481f1e7c9bfc9c955b41d688e09
xDanyBoyx <daalbarrerave@ittepic.edu.mx>
2026-02-28 23:54:17 -0700

    Primer commit: Pantalla de Login ChilaQueen

---
+++ lib/login.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:sprint2_chilaqueen/ventana1.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Chilaqueen extends StatefulWidget {
+  const Chilaqueen({super.key});
+
+  @override
+  State<Chilaqueen> createState() => _ChilaqueenState();
+}
+
+final user = TextEditingController();
+final pass = TextEditingController();
+const Color colorPrincipal = Color(0xFF1A1A1A);
+const Color colorFuente = Color(0xFFD4AF37);
+
+class _ChilaqueenState extends State<Chilaqueen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: colorPrincipal,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Image.asset("assets/logo_2.png", width: 400),
+              const SizedBox(height: 20),
+              Text(
+                "Iniciar Sesión",
+                style: GoogleFonts.playfairDisplay(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: colorFuente,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Card(
+                  color: colorPrincipal,
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(color: colorFuente, width: 0.5),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "USUARIO: ",
+                          style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.bold,
+                            color: colorFuente,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: user,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            hintText: "Nombre de usuario",
+                            hintStyle: TextStyle(color: colorFuente.withOpacity(0.6)),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: colorFuente),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: colorFuente, width: 2),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          "CONTRASEÑA: ",
+                          style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.bold,
+                            color: colorFuente,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: pass,
+                          obscureText: true,
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            hintText: "Contraseña",
+                            hintStyle: TextStyle(color: colorFuente.withOpacity(0.6)),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: colorFuente),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: colorFuente, width: 2),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const Ventana1(),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: colorFuente,
+                            ),
+                            child: const Text(
+                              "INGRESAR",
+                              style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 5,),
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const Ventana1(),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black,
+                            ),
+                            child: const Text(
+                              "REGISTRARSE",
+                              style: TextStyle(color: colorFuente, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "¿OLVIDASTE TU CONTRASEÑA?",
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.bold,
+                    color: colorFuente,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
